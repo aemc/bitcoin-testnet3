01bff637eebb8d0bbadbf2423cf99fb2bfc26f9c
aemc <aemlcruz@gmail.com>
2018-04-04 00:12:58 -0400

    Add html + routes logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const router = express.Router();
+const router  = express.Router();
+const request = require('request');
+const data = require('../creds.json');
 
 // Home page
 router.get('/', (req, res) => {
-    res.render('home');
+    request
+        .get('https://api.blockcypher.com/v1/bcy/test/addrs/' + data[0].addr + '/balance', (err, resp, body) => {
+            if (!err && resp.statusCode === 200) {
+                const balance = JSON.parse(body).final_balance;
+                res.render('home', {balance})
+            }
+        });
 });
 
 // Receive page
